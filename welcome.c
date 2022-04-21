fd229a47eea78ad45ad7ae4bda39af57d1ce741f
Ritesh Raj Pandit <wooliecodes@gmail.com>
2022-04-21 15:20:23 +0545

    add numbers

---
+++ welcome.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a, b, sum;
+    printf("Enter a and b to add: ");
+    scanf("%d%d",&a,&b);
+    sum = a+b;
+    printf("Sum of a and b: %d ",sum);
+    return 0;
+}
